1df6f7b30846e0f041bfc3e029e04c9513242e23
Fahmid Uddin <fahmiduddin@Fahmids-MacBook-Air.local>
2021-05-30 13:39:24 +0600

    Initial project from the Flow quickstart guide

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {AuthCluster} from "./auth-cluster"
+
+function App() {
+  return (
+    <div className="App">
+      <AuthCluster/>
+    </div>
+  );
+}
+
+export default App;
